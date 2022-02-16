2be4a919a16c2442eb929544bdea44ce16b1f952
Meron <meriab1234@gmail.com>
2022-02-16 09:00:21 +0300

    fixed fonts and card size

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,9 +5,9 @@ export const Card = props => (
     <div className='card-container'>
         <img
         alt='monster'
-        src={`https://robohash.org/${props.monster.id}?set=set2`}
+        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
         />
-        <h1> {props.monster.name} </h1>
+        <h3> {props.monster.name} </h3>
         <p>{props.monster.email}</p>
     </div>
 );
